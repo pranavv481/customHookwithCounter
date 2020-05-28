46c5b23a2ed994ad1ee14ce31fb09b0f47a27cd4
pranavv481 <pranavv481@gmail.com>
2020-05-28 08:52:16 +0530

    custom hook and useCounter

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import CounterOne from './components/CounterOne';
+import CounterTwo from './components/CounterTwo';
+
+
+
+
+function App() {
+  return (
+
+    <div className="App">
+      <CounterOne />
+      <CounterTwo />
+    </div >
+
+  );
+}
+
+export default App;
